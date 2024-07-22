e795285f9be2f2ef1cc5128dd239326158f45162
tevidebaek <71346095+tevidebaek@users.noreply.github.com>
2024-07-22 10:31:23 -0400

    Update test_gsd.py

--- test_gsd.py
+++ test_gsd.py
@@ -10,3 +10,8 @@ traj_file = './SimulationOutput/Side1/trajectory.gsd'
 gsd_file = gsd.hoomd.open(traj_file)
 
 print(len(gsd_file))
+
+#try to look at the index of partciles with type "A1", which is an interaction on side 1
+A_index = np.where(np.array(snap.particles.types) == "A1")[0]
+
+print(A_index)
